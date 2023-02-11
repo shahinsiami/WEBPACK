2ccf0b527a91da4efd754d9432aa8e21005de055
shahinsiami <shahinsiami@hotmail.com>
2023-02-11 20:33:28 +0330

    config webpack

---
+++ src/js/script.js
@@ -0,0 +1,3 @@
+import '../style/main.css';
+import '../style/main.scss';
+console.log('hi')
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require('path');
 
+
 module.exports = {
-    mode: 'development',
+
     entry: {
-        boundle: path.resolve(__dirname,'src/script.js')
+        boundle: path.resolve(__dirname,'src/js/script.js'),
     },
-      output: {
+    output: {
       path: path.resolve(__dirname, 'dist'),
       filename: '[name].js',
+    },
+
+
+   
+    module: {
+      rules: [
+        {
+          test: [/\.css$/i, /\.s[ac]ss$/i],
+          use: ["style-loader", "css-loader", "sass-loader",],
+        },
+      ],
     },
     
   };
